2ca1f70407793ed2335ff3e279982b89ed73f1ac
Lucia Alday <lalday@arizona.edu>
2026-03-16 09:49:37 -0700

    tictactoe gameplay functional, win condition check in progress

--- client/src/pages/UltimateTicTacToe.jsx
+++ client/src/pages/UltimateTicTacToe.jsx
@@ -7,20 +7,14 @@ export default function UltimateTicTacToe() {
     const [playerOneName, setPlayerOneName] = useState('Player One');
     const [playerTwoName, setPlayerTwoName] = useState('Player Two');
     const [turn, setTurn] = useState(0);
-    const [message, setMessage] = useState('');
+    const [square, setSquare] = useState(0);
 
     // board
     const [board, setBoard] = useState(Array(9).fill(null).map(() => ({
         plays: Array(9).fill(null),
     })));
     const [wins, setWins] = useState(Array(9).fill(null));
-
-    useEffect(() => {
-        if (checkParentWin()) {
-            return;
-        }
-        if (turn >= 81) gameDraw();
-    }, [turn]);
+    const [message, setMessage] = useState('');
 
     const handleTurn = (i, j) => {
         if (board[i].plays[j]) return; // already filled
@@ -32,37 +26,60 @@ export default function UltimateTicTacToe() {
         });
         setBoard(newBoard);
         setTurn(turn + 1);
-        checkChildWin(i);
+        setSquare(j);
+        checkChildWin(i, newBoard);
     }
 
-    const checkChildWin = (i) => {
-        if (wins[i] !== null) return;
+    const checkChildWin = (i, newBoard) => { // receive newBoard
+        if (wins[i]) return;
+        const p = newBoard[i].plays; // use newBoard, not board
         let curr = false;
-        if (board[i].plays[0] === board[i].plays[4] === board[i].plays[8]
-        ||  board[i].plays[2] === board[i].plays[4] === board[i].plays[6]
-        ||  board[i].plays[0] === board[i].plays[1] === board[i].plays[2]
-        ||  board[i].plays[3] === board[i].plays[4] === board[i].plays[5]
-        ||  board[i].plays[6] === board[i].plays[7] === board[i].plays[8]
-        ||  board[i].plays[0] === board[i].plays[3] === board[i].plays[6]
-        ||  board[i].plays[1] === board[i].plays[4] === board[i].plays[7]
-        ||  board[i].plays[2] === board[i].plays[5] === board[i].plays[8]
+        if (   (p[0] && p[0]===p[4] && p[4]===p[8])
+            || (p[0] && p[0]===p[1] && p[1]===p[2])
+            || (p[0] && p[0]===p[3] && p[3]===p[6])
+            || (p[4] && p[2]===p[4] && p[4]===p[6])
+            || (p[3] && p[3]===p[4] && p[4]===p[5])
+            || (p[1] && p[1]===p[4] && p[4]===p[7])
+            || (p[6] && p[6]===p[7] && p[7]===p[8])
+            || (p[2] && p[2]===p[5] && p[5]===p[8])
         ) {
-            setMessage('Square claimed'); // check for whether or not the condition is ever checked
             curr = true;
         }
         if (curr) {
-            let newWins = wins;
-            newWins[i] = turn % 2 === 0 ? 'x-win' : 'o-win';
+            const newWins = [...wins];
+            newWins[i] = turn % 2 === 1 ? 'x-win' : 'o-win';
             setWins(newWins);
         }
     }
 
-    const checkParentWin = () => {
+    const gameEnd = () => {
 
     }
-    const gameDraw = () => {
 
-    }
+    const getWinner = (wins) => {
+        const lines = [
+            [0,1,2],[0,3,6],[0,4,8],
+            [3,4,5],[6,7,8],[1,4,7],
+            [2,5,8],[2,4,6]
+        ];
+        for (const [a,b,c] of lines) {
+            if (wins[a] && wins[a]===wins[b] && wins[b]===wins[c]) {
+                setMessage(`${wins[a][0]} wins!`)
+                return `${wins[a][0]}`;
+            }
+        }
+        if (turn >= 81) {
+            setMessage('Draw! At least one of you needs to get better');
+            return 'Draw';
+        }
+        return null;
+    };
+
+    const gameover = getWinner(wins);
+
+    useEffect(() => {
+        if (gameover) gameEnd();
+    })
     
     return (
         <div>
@@ -77,29 +94,34 @@ export default function UltimateTicTacToe() {
                     <br></br>
                     <button onClick={()=>setStart(false)}>Start game!</button>
                 </div>
-                : <div>
+                : (message ?
+                    <div>
+                        <h2>{message}</h2>
+                    </div>
+                    :<div>
+                    
                     <div>
                         <h4>{turn % 2 == 0 ? playerOneName : playerTwoName}'s Turn! {turn}</h4>
-                        <p>{message}</p>
                     </div>
                     <br></br>
                     <div className='parent-grid'>
                         {board.map((key, i) =>
-                        <div className={`child-grid ${i<3 && 'top'} ${i>5 && 'bottom'} ${i%3==0 && 'left'} ${(i+1)%3==0 && 'right'} ${wins[i]}`}>
-                            {key.plays.map((play, j) => 
-                                <button 
+                        <div className={`child-grid ${i<3 && 'top'} ${i>5 && 'bottom'} ${i%3==0 && 'left'} ${(i+1)%3==0 && 'right'} ${wins[i]} ${square===i && 'disabled'}`}>
+                            {key.plays.map((play, j) =>
+                                <button
                                 className={`square ${j<3 && 'top'} ${j>5 && 'bottom'} ${j%3==0 && 'left'} ${(j+1)%3==0 && 'right'}`}
                                 onClick={()=>handleTurn(i, j)}
+                                disabled={square!==null && square!==i}
                                 >
                                     {play}
                                 </button>
-                            )}
-                        </div>
                         )}
+                        </div>
+                    )}
                     </div>
-                </div>}
+                </div>
+            )}
             </article>
-
         </div>
     )
 }
